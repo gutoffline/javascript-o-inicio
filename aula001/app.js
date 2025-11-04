20d831350e276aa4a91dd3c53b35a31fc86d08cc
Guto Xavier <gutoffline@gmail.com>
2025-11-03 22:26:01 -0300

    Aula 01
    - Boas práticas
    - Case sensitive
    - Sintaxe básica e comentário
    - Declaração de variáveis
    - Console e Debug
    - Tipos de dados

---
+++ aula001/app.js
@@ -0,0 +1,74 @@
+// Boas práticas
+/*
+- nomes significativos
+- função não muito longas
+*/
+
+// Case sensitive
+/* Diferenciar maiúsculas de minúsculas, Isso é diferente de isso */
+console.log("olá mundo!")
+
+// Sintaxe básica e comentário
+// uma linha
+/*
+várias
+linhas
+*/
+
+// Declaração de variáveis(var, let, const)
+let nome = "Guto"
+console.log("olá " + nome)
+nome = "Xavier"
+console.log(nome)
+
+const idade = 39
+console.log(idade)
+
+// Console e Debug
+// console.warn("atenção")
+// console.info("informações")
+// console.error("erro")
+
+
+//Tipos de dados
+/*
+número - number
+booleano - bool
+texto - string
+*/
+
+//string
+let cidade = "Americana"
+
+//number
+let salario = 2000
+
+//bool
+
+let fumante = false
+
+console.log(typeof cidade)
+console.log(typeof salario)
+console.log(typeof fumante)
+
+console.log("----------------")
+salario = "2000"
+console.log(typeof salario)
+
+// Operadores
+//     Operador Atribuição
+
+//     Operadores aritméticos
+
+//     Operadores relacionais
+
+//     Operadores lógicos
+
+// Estrutura
+//     Estrutura de controle/decisão
+
+//     Laços de repetição
+
+// Arrays --> vetor
+
+// Funções personalizadas
